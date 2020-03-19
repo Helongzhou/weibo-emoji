57dda95ff8eadc51504665af6ce98d6026c04403
zhouhelong <zhouhlwork@foxmail.com>
2020-03-19 15:43:23 +0800

    init

--- src/compontent/handle.js
+++
@@ -1,8 +0,0 @@
-export function getCategory(data) {
-    let cats = data.category;
-    return cats;
-}
-export function getEmotions(data) {
-    let emotions = data.emotions;
-    return emotions;
-}
